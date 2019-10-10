b8e74aa365008e4e9f0ad2f5fd2ba351fcb7eab5
AdminingSS <admining@mail.ru>
2019-10-10 20:35:25 +0300

    4 alternative

--- src/scripts/app/index.js
+++ src/scripts/app/index.js
@@ -1,27 +1,80 @@
 $(()=>{
     //image switcher
     (() => {
-        const $imgSwitchTop = $('.tm-active-image-top .tm-active-image');
-        const $imgSwitchBottom = $('.tm-active-image-bottom .tm-active-image');
+        const $imgSwitchTop = $('.tm-active-image-top.tm-desctop-AI .tm-active-image');
+        const $imgSwitchBottom = $('.tm-active-image-bottom.tm-desctop-AI .tm-active-image');
         const $imgTriggerTop = $('.tm-image-trigger');
         const $imgTriggerBottom = $('.tm-image-trigger-bottom');
 
-        $imgTriggerTop.on('click', function () {
-            console.log($(this));
-            const $elemIndex = $(this).index() - 6;
-            console.log($elemIndex);
-            const $elem = $($imgSwitchTop[$elemIndex]);
+        const $imgSwitchTopM = $('.tm-active-image-top.tm-mobile-AI .tm-active-image');
+        const $imgSwitchBottomM = $('.tm-active-image-bottom.tm-mobile-AI .tm-active-image');
+
+        const $imgTriggerTopMM = $('.tm-images-mobile .tm-image-trigger');
+        const $imgTriggerBottomMM = $('.tm-images-mobile .tm-image-trigger-bottom');
+
+        const $imgSwitchTopMM = $('.tm-images-mobile .tm-active-image-top.tm-mobile-AI .tm-active-image');
+        const $imgSwitchBottomMM = $('.tm-images-mobile .tm-active-image-bottom.tm-mobile-AI .tm-active-image');
+
+        $imgTriggerTopMM.on('click', function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            const $elemIndex = $(this).index() - 4;
+
+            const $elem = $($imgSwitchTopMM[$elemIndex]);
+
             if($elem.hasClass('uk-active')) return;
-            $imgSwitchTop.removeClass('uk-active');
+
+            $imgSwitchTopMM.removeClass('uk-active');
             $elem.addClass('uk-active');
         });
 
-        $imgTriggerBottom.on('click', function () {
-            const $elemIndex = $(this).index() - 6;
-            const $elem = $($imgSwitchBottom[$elemIndex]);
+        $imgTriggerBottomMM.on('click', function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            const $elemIndex = $(this).index() - 4;
+
+            const $elem = $($imgSwitchBottomMM[$elemIndex]);
+
             if($elem.hasClass('uk-active')) return;
+
+            $imgSwitchBottomMM.removeClass('uk-active');
+            $elem.addClass('uk-active');
+        });
+
+        $imgTriggerTop.on('click', function () {
+
+            const $elemIndex = $(this).index() - 6;
+            const $elemIndexM = $(this).index() - 4;
+
+            const $elem = $($imgSwitchTop[$elemIndex]);
+            const $elemM = $($imgSwitchTopM[$elemIndexM]);
+
+            if($elem.hasClass('uk-active') || $elemM.hasClass('uk-active')) return;
+
+            $imgSwitchTop.removeClass('uk-active');
+            $elem.addClass('uk-active');
+
+            $imgSwitchTopM.removeClass('uk-active');
+            $elemM.addClass('uk-active');
+        });
+
+        $imgTriggerBottom.on('click', function () {
+
+            const $elemIndex = $(this).index() - 6;
+            const $elemIndexM = $(this).index() - 4;
+
+            const $elem = $($imgSwitchBottom[$elemIndex]);
+            const $elemM = $($imgSwitchBottomM[$elemIndexM]);
+
+            if($elem.hasClass('uk-active') || $elemM.hasClass('uk-active')) return;
+
             $imgSwitchBottom.removeClass('uk-active');
             $elem.addClass('uk-active');
+
+            $imgSwitchBottomM.removeClass('uk-active');
+            $elemM.addClass('uk-active');
         });
     })();
 });
